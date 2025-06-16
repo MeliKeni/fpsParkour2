41ff413f8170be52c86a52057e8f4a4b00405817
MeliKeni <melinakenigsberg@gmail.com>
2025-06-16 13:08:05 -0300

    Clase 2

---
+++ Assets/Scripts/RestartOnFall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RestartOnFall : MonoBehaviour
+{
+public float alturaDeMuerte;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < alturaDeMuerte){//como el script esta en el mismo componente que el transform puedo directamente llamarlo usando el transform en minuscula
+            SceneManager.LoadScene("FPSParkour");
+        } 
+    }
+}
