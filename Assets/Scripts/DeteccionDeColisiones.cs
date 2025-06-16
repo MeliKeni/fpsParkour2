1ede0fda82c1b41d12c3648733d7c2e14f04c21a
MeliKeni <melinakenigsberg@gmail.com>
2025-06-16 15:32:20 -0300

    Clase 4, collisions

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision){ //Es una funcion que ya existe, es par apenas se tocan, tenes OnCollisionExit que es apenas termina y OnCollisionStay
+        Destroy(gameObject); // gameObject hace referencia al objeto que tiene el script asignado 
+        Destroy(collision.gameObject); //como tenes el collision.gameObject estas eliminando el objeto con el que colisiona
+    } 
+}
