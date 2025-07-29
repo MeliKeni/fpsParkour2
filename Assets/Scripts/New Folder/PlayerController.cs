1122d1b70d6281174711eebb9e3d2389f696281a
MeliKeni <melinakenigsberg@gmail.com>
2025-07-29 18:17:29 -0300

    Checkpoint reviven

---
+++ Assets/Scripts/New Folder/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        // Si el jugador cae por debajo de Y = -30
+        if (transform.position.y < -30f)
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        // Teletransporta al jugador a la posición guardada en Checkpoint
+        transform.position = CheckpointTrigger.currentCheckpointPosition;
+        Debug.Log("Jugador reapareció en checkpoint.");
+        
+        // Opcional: si tienes Rigidbody, resetear velocidad
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if(rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
